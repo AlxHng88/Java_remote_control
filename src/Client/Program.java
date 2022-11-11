00d539ed14311fe1ccc7d3de3044ea9dbdcdd36c
AlxHng88 <Alexandros8808@gmail.com>
2022-11-11 23:48:39 +0700

    11:48 11/11/2022

--- src/Client/Program.java
+++ src/Client/Program.java
@@ -3,9 +3,7 @@ package Client;
 public class Program {
 
     public static void main(String[] args){
-        System.out.println("Hello World!");
-        System.out.println("test github1");
-        System.out.println("test github2");
+
     }
 
 }
