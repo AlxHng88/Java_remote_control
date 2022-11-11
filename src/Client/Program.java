3e93130c6e14b2a625c14e265c58bee1f01c9ceb
AlxHng88 <Alexandros8808@gmail.com>
2022-11-11 23:57:20 +0700

    11:48 11/11/2022

--- src/Client/Program.java
+++ src/Client/Program.java
@@ -1,9 +1,22 @@
 package Client;
 
+import Client.view.client;
+
+import java.awt.*;
+
 public class Program {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    client frame = new client();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 }
---
+++ src/Client/view/client.java
@@ -0,0 +1,66 @@
+package Client.view;
+
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class client extends JFrame {
+
+    private JPanel contentPane;
+    private JTextField txtIP;
+
+    /**
+     * Create the frame.
+     */
+    public client() {
+        setTitle("Client");
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 452, 284);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        txtIP = new JTextField();
+        txtIP.setText(" Enter IP:");
+        txtIP.setBounds(20, 10, 305, 34);
+        contentPane.add(txtIP);
+        txtIP.setColumns(10);
+
+        JButton btnConnect = new JButton("Connect");
+        btnConnect.setBounds(335, 9, 85, 35);
+        contentPane.add(btnConnect);
+
+        JButton btnApp = new JButton("App Running");
+        btnApp.setBounds(20, 54, 140, 80);
+        contentPane.add(btnApp);
+
+        JButton btnKeystroke = new JButton("Keystroke");
+        btnKeystroke.setBounds(170, 54, 120, 80);
+        contentPane.add(btnKeystroke);
+
+        JButton btnCapture = new JButton("Capture");
+        btnCapture.setBounds(300, 54, 120, 80);
+        contentPane.add(btnCapture);
+
+        JButton btnProcess = new JButton("Process Running");
+        btnProcess.setBounds(20, 144, 140, 80);
+        contentPane.add(btnProcess);
+
+        JButton btnShutdown = new JButton("Shutdown");
+        btnShutdown.setBounds(170, 144, 120, 80);
+        contentPane.add(btnShutdown);
+
+        JButton btnExit = new JButton("Exit");
+        btnExit.setBounds(300, 144, 120, 80);
+        contentPane.add(btnExit);
+    }
+}
+
